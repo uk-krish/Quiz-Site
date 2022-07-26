33fac34491cbc0b1f8d797830259c87de0576ae8
Unnikrishnan.c <66357763+uk-krish@users.noreply.github.com>
2022-07-26 17:24:25 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a Java keyword?",
+    answer: "new",
+    options: [
+      "static",
+      "try",
+      "Integer",
+      "new"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The \u0021 article referred to as a ?",
+    answer: "Unicode escape sequence",
+    options: [
+      "Unicode escape sequence",
+      "Octal escape",
+      "Hexadecimal",
+      "Line feed"
+    ]
+  },
+    {
+    numb: 3,
+    question: "_____ is used to find and fix bugs in the Java programs.",
+    answer: "JDB",
+    options: [
+      "JVM",
+      "JRE",
+      "JDK",
+      "JDB"
+    ]
+  },
+    {
+    numb: 4,
+    question: " What does the expression float a = 35 / 0 return?",
+    answer: "Infinity",
+    options: [
+      "0",
+      "Not a Number",
+      "Infinity",
+      "Run time exception"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?",
+    answer: "Javadoc tool",
+    options: [
+      "javap tool",
+      "javaw command",
+      "Javadoc tool",
+      "javah command"
+    ]
+  },
+];
